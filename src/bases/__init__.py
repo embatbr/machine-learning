51061db4abc511864dab45c5d23c56a8025a271c
Eduardo Tenório <embatbr@gmail.com>
2016-06-22 04:17:56 -0300

    Multinomial Naives Bayes working.
    Signed-off-by: Eduardo Tenório <embatbr@gmail.com>

---
+++ src/bases/__init__.py
@@ -0,0 +1,7 @@
+"""Package to read bases
+"""
+
+
+from bases.mnist import read_images_file as read_MNIST_images
+from bases.mnist import read_labels_file as read_MNIST_labels
+from bases.mnist import extract_features as extract_MNIST_features
--- src/digits/naive_bayes.py
+++ src/digits/naive_bayes.py
@@ -11,7 +11,7 @@ import numpy as np
 import sys, os
 sys.path.append(os.path.abspath('..'))
 
-import datasets
+import bases
 
 
 def calculate_prior_probs(lb_info):
@@ -28,7 +28,77 @@ def calculate_prior_probs(lb_info):
     return prior_probs
 
 
-class GaussianNB(object):
+def accuracy(prediction, labels):
+    equals = prediction == labels
+    return len(equals[equals]) / len(labels)
+
+
+class NB(object):
+
+    def __init__(self, num_classes, prior_probs, feature_dim):
+        self.num_classes = num_classes
+        self.prior_probs = prior_probs
+
+    def fit(self, features, labels, label_names):
+        pass
+
+    def predict(self, features_row, label_names):
+        pass
+
+    def predict_many(self, features, label_names):
+        pass
+
+
+class MultinomialNB(NB):
+
+    def __init__(self, num_classes, prior_probs, feature_dim):
+        super().__init__(num_classes, prior_probs, feature_dim)
+
+        self.likelihoods = np.zeros((self.num_classes, feature_dim))
+
+
+    def fit(self, features, labels, label_names, alpha=1):
+        num_label_names = len(label_names)
+        alpha_denom = alpha * features.shape[1]
+
+        for i in range(num_label_names):
+            label_name = label_names[i]
+            indices = np.where(labels == label_name)[0]
+            flt_features = features[indices]
+
+            self.likelihoods[i] = np.sum(flt_features, axis=0) + alpha
+            self.likelihoods[i] = self.likelihoods[i] / (np.sum(flt_features) + alpha_denom)
+
+
+    def predict(self, features_row, label_names):
+        indices = np.where(features_row == 1)[0]
+
+        max_index = -1
+        max_post_prob = -sys.maxsize
+
+        for i in range(self.num_classes):
+            flt_likelihoods = self.likelihoods[i][indices]
+            post_prob = np.sum(np.log(flt_likelihoods)) + np.log(self.prior_probs[i])
+
+            if post_prob > max_post_prob:
+                max_post_prob = post_prob
+                max_index = i
+
+        return label_names[max_index]
+
+
+    def predict_many(self, features, label_names):
+        num_features = len(features)
+        prediction = np.empty(num_features)
+
+        for n in range(num_features):
+            features_row = features[n]
+            prediction[n] = self.predict(features_row, label_names)
+
+        return prediction
+
+
+class GaussianNB(NB):
 
     def __init__(self, num_classes, prior_probs, feature_dim):
         self.num_classes = num_classes
@@ -40,7 +110,7 @@ class GaussianNB(object):
     def fit(self, features, labels, label_names):
         for i in range(self.num_classes):
             label_name = label_names[i]
-            indices = np.where((labels - label_name) == 0)[0]
+            indices = np.where(labels == label_name)[0]
             flt_features = features[indices]
 
             self.means[i] = np.mean(flt_features, axis=0)
@@ -51,16 +121,30 @@ if __name__ == '__main__':
     DATASET_DIR = '../../datasets/MNIST'
 
     # training data
-    training_img_info = datasets.read_images_file('%s/training.images' % DATASET_DIR)
-    training_lb_info = datasets.read_labels_file('%s/training.labels' % DATASET_DIR)
+    training_img_info = bases.read_MNIST_images('%s/training.images' % DATASET_DIR)
+    training_lb_info = bases.read_MNIST_labels('%s/training.labels' % DATASET_DIR)
 
     # test data
-    test_img_info = datasets.read_images_file('%s/test.images' % DATASET_DIR)
-    test_lb_info = datasets.read_labels_file('%s/test.labels' % DATASET_DIR)
+    test_img_info = bases.read_MNIST_images('%s/test.images' % DATASET_DIR)
+    test_lb_info = bases.read_MNIST_labels('%s/test.labels' % DATASET_DIR)
 
     num_classes = training_lb_info['num_label_names']
     prior_probs = calculate_prior_probs(training_lb_info)
-    features = datasets.extract_features(training_img_info)
+    features_training = bases.extract_MNIST_features(training_img_info)
+    features_test = bases.extract_MNIST_features(test_img_info)
 
-    gnb = GaussianNB(num_classes, prior_probs, features.shape[1])
-    gnb.fit(features, training_lb_info['labels'], training_lb_info['label_names'])
+    labels_training = training_lb_info['labels']
+    label_names_training = training_lb_info['label_names']
+    labels_test = test_lb_info['labels']
+    label_names_test = test_lb_info['label_names']
+
+    mnb = MultinomialNB(num_classes, prior_probs, features_training.shape[1])
+    mnb.fit(features_training, labels_training, label_names_training)
+
+    prediction = mnb.predict_many(features_training, label_names_training)
+    acc = accuracy(prediction, labels_training)
+    print('accuracy (training): %f' % acc)
+
+    prediction = mnb.predict_many(features_test, label_names_test)
+    acc = accuracy(prediction, labels_test)
+    print('accuracy (test): %f' % acc)
